4bbeb6296ac326c71f239002e8671558734fe14c
cheezihoe <cheezihoe@gmail.com>
2023-10-28 20:50:52 +0800

    metamask fix

--- public/app.js
+++ public/app.js
@@ -1,30 +1,22 @@
 window.addEventListener('load', async () => {
-    if (typeof web3 !== 'undefined') {
-        // Use web3 to connect to the blockchain
-        const web3Provider = new Web3(web3.currentProvider);
-        const accounts = await web3Provider.eth.getAccounts();
-        const accountAddress = accounts[0];
+    if (window.ethereum) {
+        const web3Provider = new Web3(window.ethereum);
 
-        document.getElementById('account-address').textContent = accountAddress;
-
-        // Add event listeners for button clicks
+        // Add event listener for the "Connect Wallet" button
         document.getElementById('connect-wallet').addEventListener('click', async () => {
             try {
-                if (window.ethereum) {
-                    // Request the user's permission to connect to MetaMask
-                    await window.ethereum.request({ method: 'eth_requestAccounts' });
-                    const accounts = await web3Provider.eth.getAccounts();
-                    const accountAddress = accounts[0];
-                    document.getElementById('account-address').textContent = accountAddress;
-                    console.log('Connected to MetaMask!');
-                } else {
-                    console.error('MetaMask is not installed.');
-                }
+                // Request the user's permission to connect to MetaMask
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                const accounts = await web3Provider.eth.getAccounts();
+                const accountAddress = accounts[0];
+                document.getElementById('account-address').textContent = accountAddress;
+                console.log('Connected to MetaMask!');
             } catch (error) {
                 console.error('Connection to MetaMask failed:', error);
             }
         });
 
+        // Add event listeners for other button clicks
         document.getElementById('place-order').addEventListener('click', () => {
             // Implement order placement logic
         });
@@ -32,6 +24,6 @@ window.addEventListener('load', async () => {
         // Fetch and display order book data
         // You will need to retrieve and display order book data here
     } else {
-        alert('Please install a web3-enabled browser or extension.');
+        alert('Please install the MetaMask extension for Google Chrome.');
     }
 });
