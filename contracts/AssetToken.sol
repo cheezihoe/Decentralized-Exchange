e8ebf1e8a60d41d12aacf7869c9749f2e24ae478
jw-rgb <jw123040@gmail.com>
2023-11-07 21:36:11 +0800

    -

--- contracts/AssetToken.sol
+++ contracts/AssetToken.sol
@@ -1,2 +1,67 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity >=0.8.0 <=0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 ;
+
+// Import the ERC20 token contract from OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Define a new contract called AssetIssuerToken that inherits from ERC20.
+contract AssetIssuerToken is ERC20 {
+    // Declare a state variable to store the owner's address.
+    address public owner;
+
+    // Constructor function that initializes the token with a name, symbol, and initial supply.
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        // Set the owner as the address that deploys the contract.
+        owner = msg.sender;
+
+        // Mint the initial supply of tokens and assign them to the owner.
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Modifier to restrict functions to be called only by the contract owner.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    // Function to mint new tokens and assign them to a specified account.
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    // Function to burn (destroy) tokens from a specified account.
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
+
+//for testing
+contract Token2 is ERC20 {
+    // Declare a state variable to store the owner's address.
+    address public owner;
+
+    // Constructor function that initializes the token with a name, symbol, and initial supply.
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        // Set the owner as the address that deploys the contract.
+        owner = msg.sender;
+
+        // Mint the initial supply of tokens and assign them to the owner.
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Modifier to restrict functions to be called only by the contract owner.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    // Function to mint new tokens and assign them to a specified account.
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    // Function to burn (destroy) tokens from a specified account.
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
--- contracts/Orderbook.sol
+++ contracts/Orderbook.sol
@@ -10,7 +10,10 @@ contract Orderbook {
         uint id;
         address wallet;
         uint price;
+        //quantity is quantity of base token
         uint quantity;
+        bool isFilled;
+        bool isbuyOrder;
         address baseToken;
         address quoteToken;
     }
@@ -18,34 +21,54 @@ contract Orderbook {
     Order[] public buyOrders;
     Order[] public sellOrders;
     
+    event TradeMatched (
+        uint buyOrderID,
+        uint sellOrderID,
+        address buyerWallet,
+        address sellerWallet,
+        uint price,
+        uint quantity
+    );
+
+    event OrderCancelled (
+        uint OrderId,
+        address wallet,
+        bool isBuyOrder
+    );
+
     //Place buy limit order
-    function buyLimitOrder(uint _price , uint _quantity, address _baseToken, address _quoteToken) public {
+    function buyLimitOrder(uint _price , uint _quantity, address _baseToken, address _quoteToken) external {
         require (_price > 0 , "Price must be greater than zero");
         require (_quantity > 0, "Quantity must be greater than zero");
-        
+
+        uint orderValue = _price * _quantity;
+        require(IERC20(_quoteToken).allowance(msg.sender, address(this)) >= orderValue, "Allowance not enough");
         uint id = buyOrders.length;
-        Order memory newOrder = Order(id,msg.sender,_price, _quantity, _baseToken,_quoteToken);
+        Order memory newOrder = Order(id,msg.sender,_price, _quantity, false,true, _baseToken,_quoteToken);
         insertBuyOrder(newOrder);
         matchBuyOrders(findCorrectBuyOrder(newOrder.id));
   
     }
 
-    function sellLimitOrder(uint _price , uint _quantity, address _baseToken, address _quoteToken) public {
+    function sellLimitOrder(uint _price , uint _quantity, address _baseToken, address _quoteToken) external {
         require (_price > 0 , "Price must be greater than zero");
         require (_quantity > 0, "Quantity must be greater than zero");
-        
-        //Order id start from 1
-        uint id = buyOrders.length + 1;
-        insertSellOrder(Order(id,msg.sender,_price, _quantity, _baseToken,_quoteToken));
+        require(IERC20(_baseToken).allowance(msg.sender, address(this)) >= _quantity, "Allowance not enough");
+
+        uint id = sellOrders.length;
+        Order memory newOrder = Order(id,msg.sender,_price, _quantity, false,false, _baseToken,_quoteToken);
+        insertSellOrder(newOrder);
+        matchSellOrders(findCorrectSellOrder(newOrder.id));
+  
  
     }
 
     function matchBuyOrders(uint buyOrderPlacement) internal {
         Order storage buyOrder = buyOrders[buyOrderPlacement];
 
-        for (uint i = 0; i < sellOrders.length; i++){
-            Order memory sellOrder = sellOrders[i];
-            if (sellOrder.price == buyOrder.price){
+        for (uint i = 0; i < sellOrders.length && !buyOrder.isFilled; i++){
+            Order storage sellOrder = sellOrders[i];
+            if ((sellOrder.price <= buyOrder.price) && !sellOrder.isFilled){
                 uint minQty = min(sellOrder.quantity, buyOrder.quantity);
                 uint tradeValue = minQty * buyOrder.price;
 
@@ -61,12 +84,71 @@ contract Orderbook {
 
                 //Check if Orders Completely Filled
                 if (buyOrder.quantity == 0){
-                    RemoveBuyOrder(buyOrderPlacement);
+                    buyOrder.isFilled = true;
                 }
+
+                if (sellOrder.quantity == 0){
+                    sellOrder.isFilled = true;
+                }
+
+                emit TradeMatched(buyOrder.id, sellOrder.id, buyOrder.wallet, sellOrder.wallet, buyOrder.price, minQty);
             }
         }
         
         
+    }
+
+    function matchSellOrders(uint sellorderPlacement) internal {
+        Order storage sellOrder = sellOrders[sellorderPlacement];
+
+        for (uint i = 0; i < buyOrders.length && !sellOrder.isFilled; i++){
+            Order storage buyOrder = buyOrders[i];
+            if ((buyOrder.price >= sellOrder.price) && !buyOrder.isFilled){
+                uint minQty = min(sellOrder.quantity, buyOrder.quantity);
+                uint tradeValue = minQty * sellOrder.price;
+
+                //Transfer quote token from buyer to seller
+                IERC20(sellOrder.quoteToken).transferFrom(buyOrder.wallet, sellOrder.wallet, tradeValue);
+
+                //Transfer base token from seller to buyer
+                IERC20(sellOrder.baseToken).transferFrom(sellOrder.wallet, buyOrder.wallet, minQty);
+                
+                //Update Order quantities
+                buyOrder.quantity = buyOrder.quantity - minQty;
+                sellOrder.quantity = sellOrder.quantity - minQty;
+
+                //Check if Orders Completely Filled
+                if (sellOrder.quantity == 0){
+                    sellOrder.isFilled = true;
+                }
+
+                if (buyOrder.quantity == 0){
+                    buyOrder.isFilled = true;
+                }
+
+                emit TradeMatched(buyOrder.id, sellOrder.id, buyOrder.wallet, sellOrder.wallet, buyOrder.price, minQty);
+
+            }
+        }
+
+        
+    }
+
+    //Allows users to cancel order
+    function cancelOrders (uint orderid, bool isbuyOrder) external {
+        Order storage order;
+        if (isbuyOrder == true) {
+            order = buyOrders[findCorrectBuyOrder(orderid)];
+
+        } else {
+            order = sellOrders[findCorrectSellOrder(orderid)];
+        }
+
+        require (order.wallet == msg.sender, "Not trader");
+        order.isFilled = true;
+        
+        emit OrderCancelled(orderid, msg.sender, isbuyOrder);
+
     }
 
     
@@ -94,24 +176,47 @@ contract Orderbook {
     }    
 
     function findCorrectBuyOrder(uint _buyOrderid) internal view returns (uint) {
+        require(_buyOrderid < buyOrders.length, "Buy Order id out of range");
         for (uint i = 0; i < buyOrders.length; i++){
             if(buyOrders[i].id == _buyOrderid){
                 return i;
             }
         }
-
         revert("Buy Order not found");
+
     }
 
-    function RemoveBuyOrder(uint index) internal {
-        require (index < buyOrders.length, "Invalid Index");
-        for (uint i = index; i<buyOrders.length;i++){
-            buyOrders[i] = buyOrders[i+1];
+    function findCorrectSellOrder(uint _sellOrderid) internal view returns (uint) {
+        require(_sellOrderid < sellOrders.length, "Sell Order id out of range");
+        for (uint i = 0; i < sellOrders.length; i++){
+            if(sellOrders[i].id == _sellOrderid){
+                return i;
+            }
         }
-        
-        buyOrders.pop();
 
+        revert("Sell Order not found");
     }
+
+    // function RemoveBuyOrder(uint index) internal {
+    //     require (index < buyOrders.length, "Invalid Buy Order Index");
+    //     for (uint i = index; i<buyOrders.length;i++){
+    //         buyOrders[i] = buyOrders[i+1];
+    //     }
+        
+    //     buyOrders.pop();
+
+    // }
+
+    // function RemoveSellOrder(uint index) internal {
+    //     require (index < sellOrders.length, "Invalid Sell Order Index");
+    //     for (uint i = index; i<sellOrders.length;i++){
+    //         sellOrders[i] = sellOrders[i+1];
+    //     }
+        
+    //     sellOrders.pop();
+
+    // }
+
     function min(uint a, uint b) internal pure returns (uint){
         return a < b ? a : b;
     }
