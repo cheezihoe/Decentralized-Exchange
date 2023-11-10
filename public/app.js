06a90f41d497fa2a084196fac27558c472e6e571
cheezihoe <cheezihoe@gmail.com>
2023-11-10 16:26:11 +0800

    integration

--- public/app.js
+++ public/app.js
@@ -2,6 +2,13 @@ window.addEventListener('load', async () => {
     if (window.ethereum) {
         const web3Provider = new Web3(window.ethereum);
 
+        // Load the ABI and bytecode of your AssetToken contract
+        const assetTokenAbi = [...]; // Replace with your actual ABI
+        const assetTokenBytecode = '0x...'; // Replace with your actual bytecode
+
+        // Create a contract instance for AssetToken
+        const assetTokenContract = new web3Provider.eth.Contract(assetTokenAbi);
+
         // Add event listener for the "Connect Wallet" button
         document.getElementById('connect-wallet').addEventListener('click', async () => {
             try {
@@ -16,9 +23,29 @@ window.addEventListener('load', async () => {
             }
         });
 
-        // Add event listeners for other button clicks
-        document.getElementById('place-order').addEventListener('click', () => {
-            // Implement order placement logic
+        // Add event listener for the "Issue Asset" button
+        document.getElementById('issue-asset').addEventListener('click', async () => {
+            const assetName = document.getElementById('asset-name').value;
+            const assetSymbol = document.getElementById('asset-symbol').value;
+            const issueAmount = document.getElementById('issue-amount').value;
+
+            try {
+                const accounts = await web3Provider.eth.requestAccounts();
+                const account = accounts[0];
+
+                // Deploy a new AssetToken contract
+                const deployedToken = await assetTokenContract.deploy({
+                    data: assetTokenBytecode,
+                    arguments: [assetName, assetSymbol],
+                }).send({ from: account });
+
+                // Mint new tokens
+                await deployedToken.methods.mint(account, issueAmount).send({ from: account });
+
+                console.log('Asset issued!');
+            } catch (error) {
+                console.error('Error issuing asset:', error);
+            }
         });
 
         // Fetch and display order book data
